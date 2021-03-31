69b2ef9cd68b15afcd126b3005fb4c449f443a35
Ilya Potyupkin <potypkin@mail.ru>
2021-03-31 22:44:38 +0300

    Service

---
+++ src/main/kotlin/ru/netology/pusher/newPost.kt
@@ -0,0 +1,31 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .setDatabaseUrl(dbUrl)
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val message = Message.builder()
+        .putData("action", "NEWPOST")
+        .putData("content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology",
+          "content": "Kotlin (Ко́тлин) — статически типизированный, объектно-ориентированный язык программирования, работающий поверх Java Virtual Machine и разрабатываемый компанией JetBrains. Также компилируется в JavaScript и в исполняемый код ряда платформ через инфраструктуру LLVM. Язык назван в честь острова Котлин в Финском заливе, на котором расположен город Кронштадт."
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(message)
+}
